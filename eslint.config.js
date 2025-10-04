336fc06a5ed5dbd05b864dff018635e03d360573
thuongtruong109 <thuongtruongofficial@gmail.com>
2025-10-05 00:12:13 +0700

    chore(deps): config formatter

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier';
+
+export default tseslint.config(
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    plugins: {
+      prettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-console': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        { argsIgnorePattern: '^_' },
+      ],
+    },
+    ignores: ['dist', 'node_modules'],
+  },
+);
