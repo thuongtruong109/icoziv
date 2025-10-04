b5ad04970c04176cbf5558e056ab968ace89f7de
thuongtruong109 <thuongtruongofficial@gmail.com>
2025-10-04 13:27:15 +0700

    fix: update assets path

--- build.ts
+++ build.ts
@@ -6,14 +6,15 @@ import {
   writeFileSync,
 } from 'node:fs';
 
-const iconsDir = readdirSync('./icons');
+const iconsDir = readdirSync('./public/icons');
 const icons: Record<string, string> = {};
 for (const icon of iconsDir) {
   // const name = icon.replace('.svg', '').toLowerCase();
   const name = icon.replace('.svg', '');
 
-  icons[name] = String(readFileSync(`./icons/${icon}`));
+  icons[name] = String(readFileSync(`./public/icons/${icon}`));
 }
 
-if (!existsSync('./dist')) mkdirSync('./dist');
-writeFileSync('./dist/icons.json', JSON.stringify(icons));
+// if (!existsSync('./dist')) mkdirSync('./dist');
+// writeFileSync('./dist/icons.json', JSON.stringify(icons));
+writeFileSync('./public/icons.json', JSON.stringify(icons));
