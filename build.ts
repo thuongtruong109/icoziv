5d0c5bae661325d43a601578ec5747f58d303459
thuongtruong109 <thuongtruongofficial@gmail.com>
2025-10-05 17:18:29 +0700

    fix: split build source path

--- build.ts
+++ build.ts
@@ -6,16 +6,17 @@ import {
   writeFileSync,
 } from 'node:fs';
 
-const iconsDir = readdirSync('./public/icons');
+const iconsDir = readdirSync('./icons');
 const icons1: Record<string, string> = {};
 const icons2: Record<string, string> = {};
 
 for (const icon of iconsDir) {
-  const name1 = icon.replace('.svg', '').toLowerCase();
-  const name2 = icon.replace('.svg', '');
+  const tmp = icon.replace('.svg', '');
+  const name2 = tmp;
+  const name1 = tmp.toLowerCase();
 
-  icons1[name1] = String(readFileSync(`./public/icons/${icon}`));
-  icons2[name2] = String(readFileSync(`./public/icons/${icon}`));
+  icons1[name1] = String(readFileSync(`./icons/${icon}`));
+  icons2[name2] = String(readFileSync(`./icons/${icon}`));
 }
 
 if (!existsSync('./dist')) mkdirSync('./dist');
